ffd5a41c832e861ab01cfb42a8b8021f0c5007d4
siangyeh <siangyeh@office.emotibot.com>
2018-03-25 09:51:53 +0800

    remove TBrain_FLMD_0301.sql

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 
 
 #--------------------------------------------------
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-source $DIR/../../env.sh
-if [ $? -ne 0 ]; then
-  echo "Erorr, can't open envfile: $1"
-  exit 1
-fi
+#DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+#source $DIR/../../env.sh
+#if [ $? -ne 0 ]; then
+#  echo "Erorr, can't open envfile: $1"
+#  exit 1
+#fi
 #--------------------------------------------------
 MYSQL_CONTAINER_NAME="maridb"
 MYSQL_ROOT_PASSWORD="password"
 MYSQL_DOCKER_IMG="mariadb:10.2-2018-03-25"
-DATA_PATH="/User/siangye/Document/mariadb_data_folder/"
+DATA_PATH="/Users/siangye/Documents/mariadb_data_folder/"
 #--------------------------------------------------
 docker run -d \
   --restart="always" \
